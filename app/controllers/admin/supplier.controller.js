13db58fbd61eada8bf25baab5ef9dc37516957c5
Hassib Moddasser <hassib.moddasser@gmail.com>
2022-07-04 17:02:59 +0430

    refactor: connect the controller and model of supplier

--- app/controllers/admin/supplier.controller.js
+++ app/controllers/admin/supplier.controller.js
@@ -1,10 +1,22 @@
 const asyncHandler = require("express-async-handler");
+const validUrl = require("valid-url");
 
+const Supplier = require("../../models/supplier.model");
+
+/**
+ * @module app/controllers/admin/supplier
+ * @description Get all suppliers
+ * @route GET /api/v1/admin/supplier
+ * @access private
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {Promise<Object>}
+ */
 const getSuppliers = asyncHandler(async (req, res) => {
   try {
-    res.status(200).json({
-      msg: "Suppliers fetched successfully",
-    });
+    const suppliers = await Supplier.find();
+
+    res.status(200).json(suppliers);
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -13,11 +25,47 @@ const getSuppliers = asyncHandler(async (req, res) => {
   }
 });
 
+/**
+ * @module app/controllers/admin/supplier
+ * @description Add a supplier
+ * @route POST /api/v1/admin/supplier
+ * @access private
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Object} next - Express next middleware function
+ * @returns {Promise<Object>}
+ */
 const addSupplier = asyncHandler(async (req, res) => {
+  const {
+    name, website, deliveryTime, minimumOrder,
+  } = req.body;
+
+  if (!name) {
+    res.status(400);
+    throw new Error("Name is required");
+  }
+
+  if (website && !validUrl.isUri(website)) {
+    res.status(400);
+    throw new Error("Website is not a valid URL");
+  }
+
+  if (deliveryTime && deliveryTime < 0) {
+    res.status(400);
+    throw new Error("Delivery time must be greater than 0");
+  }
+
+  if (minimumOrder && minimumOrder < 600) {
+    res.status(400);
+    throw new Error("Minimum order value must be at least 600");
+  }
+
   try {
-    res.status(201).json({
-      msg: "Supplier added successfully",
-    });
+    // Create a new supplier
+    const supplier = await Supplier.create(req.body);
+
+    // Send the supplier back to the client
+    res.status(201).json(supplier);
   } catch (error) {
     res.status(500).json({
       success: false,
