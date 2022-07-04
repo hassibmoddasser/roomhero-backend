5e7a71db98ced355bfbfe1094de53959a005e153
Hassib Moddasser <hassib.moddasser@gmail.com>
2022-07-04 16:33:45 +0430

    feat: create a mongodb connection

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// load enviroment variables from .env file
+const envConfig = dotenv.config();
+
+const { errorHandler } = require("./middlewares/error");
+const connectDB = require("./config/db");
+
+// connect to database
+connectDB();
+
 // create an Express application
 const app = express();
+
+app.use(errorHandler);
 
 module.exports = app;
