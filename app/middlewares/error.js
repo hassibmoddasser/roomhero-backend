f5cf5721e335b0680cff26eceb12bdfbab7c1564
Hassib Moddasser <hassib.moddasser@gmail.com>
2022-07-04 16:29:11 +0430

    docs: add comments to the error handler

--- app/middlewares/error.js
+++ app/middlewares/error.js
@@ -1,3 +1,14 @@
+/**
+ * @module app/middleware/error
+ * @description Error handling middleware
+ * @route GET /api/v1/admin/supplier
+ * @access private
+ * @param {Object} err - Express error object
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Object} next - Express next middleware function
+ * @returns {Promise<Object>}
+ */
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
